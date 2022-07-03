f42116d5b9450bffd0bbc92508e096bd274dcba7
Chris(PC) <bissuetox@gmail.com>
2022-07-02 21:34:37 -0700

    express file restructure

--- server.js
+++ server.js
@@ -3,14 +3,7 @@ const path = require('path')
 const app = express();
 const port = 3000;
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// app.post
-// app.put
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
